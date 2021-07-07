36565039ab0783aa7a5e9b3ed0ef5f1d9741dfb5
bscott110 <blake.scott110@gmail.com>
2021-07-07 12:10:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import praw
 
-reddit = praw.Reddit(client_id='TAsJn7Y4ArZC1A', client_secret='Pkf19s972-lShAi5CELfpjLu36JcNQ',
-                     username='xxx', password='xxx', user_agent='APITEST_0.0.1')
+with open('pw.txt') as file:
+    pw_line = file.readline()
 
-submissions = reddit.subreddit('wallstreetbets').top(limit=5)
-for submission in submissions:
+reddit = praw.Reddit(client_id='DL-0oLtcYyalnDxjY5hOcw', client_secret='odOqD6s1ser0-vkMTHGhyli_ABM99w',
+                     username='BScott110', password=pw_line, user_agent='APITEST_0.0.2')
+
+submissions0 = reddit.subreddit('wallstreetbets').top(limit=5)
+
+print("Top 5 WSB Threads:")
+for submission in submissions0:
     i = submission.title
-    print(i)
+    print("-"+i)
+
+submissions1 = reddit.subreddit('wallstreetbets').hot(limit=5)
+
+print("Hottest 5 WSB Threads:")
+for submission in submissions1:
+    i = submission.title
+    print("-"+i)
+
+submissions2 = reddit.subreddit('wallstreetbets').new(limit=5)
+
+print("Newest 5 WSB Threads:")
+for submission in submissions2:
+    i = submission.title
+    print("-"+i)
+
+submissions3 = reddit.subreddit('wallstreetbets').controversial(limit=5)
+
+print("5 Most Controversial WSB Threads:")
+for submission in submissions3:
+    i = submission.title
+    print("-"+i)
